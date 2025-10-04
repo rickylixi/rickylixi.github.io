42c64bc19d36f54105c369714a4ccb6dd912fb42
Xi <pkulixi@gmail.com>
2025-10-04 14:23:08 +0800

    update

---
+++ javascripts/accordion.js
@@ -0,0 +1,16 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel) {
+      panel.classList.toggle("show");
+    }
+  });
+}
