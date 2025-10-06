1ac13c57bd3994a899c316a9c782db7de86aee10
Xi <pkulixi@gmail.com>
2025-10-06 19:54:59 +0800

    Revert "button"
    This reverts commit 99e02468f21d0ed3162f18337b3d5636635ed740.

--- javascripts/accordion.js
+++ javascripts/accordion.js
@@ -1,11 +1,9 @@
 // Accordion functionality
-window.addEventListener('load', function() {
+document.addEventListener('DOMContentLoaded', function() {
   var acc = document.getElementsByClassName("accordion");
-  console.log("Found " + acc.length + " accordion elements.");
   
   for (var i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-      console.log("Accordion button clicked.");
       /* Toggle between adding and removing the "active" class,
       to highlight the button that controls the panel */
       this.classList.toggle("active");
@@ -13,10 +11,7 @@ window.addEventListener('load', function() {
       /* Toggle between hiding and showing the active panel */
       var panel = this.nextElementSibling;
       if (panel && panel.classList.contains("panel")) {
-        console.log("Toggling 'show' class on panel.");
         panel.classList.toggle("show");
-      } else {
-        console.log("Panel not found or is not a panel.");
       }
     });
   }
