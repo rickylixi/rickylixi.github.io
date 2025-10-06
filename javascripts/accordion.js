0bfe04fd8a0b08bc54595b60144036c2652a4bf8
Xi <pkulixi@gmail.com>
2025-10-06 20:55:54 +0800

    update

--- javascripts/accordion.js
+++ javascripts/accordion.js
@@ -1,21 +1,22 @@
 // Accordion functionality
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
   var acc = document.getElementsByClassName("accordion");
+  console.log("Found " + acc.length + " accordion elements.");
   
   for (var i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
+      console.log("Accordion button clicked.");
       /* Toggle between adding and removing the "active" class,
       to highlight the button that controls the panel */
       this.classList.toggle("active");
 
       /* Toggle between hiding and showing the active panel */
-      var panel = this.nextElementSibling;
-      if (panel && panel.classList.contains("panel")) {
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
+      var panel = this.parentElement.getElementsByClassName('panel')[0];
+      if (panel) {
+        console.log("Toggling 'show' class on panel.");
+        panel.classList.toggle("show");
+      } else {
+        console.log("Panel not found.");
       }
     });
   }
